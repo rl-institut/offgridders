ce229aaa2a238803ea6d6e9a4daa22e27303b6dc
Martha_RLI <martha.hoffmann@rl-institut.de>
2018-10-18 14:21:38 +0200

    Trying out Pandas Created csv file with costs Trying to read csv file with pandas.read_csv()

---
+++ input_read_from_files.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# read with "read_csv", fields are comma-seperated
+
+all_data = pd.read_csv('input_data_costs.csv', encoding='utf8', index_col='Cost')
+
+all_data['Investment']
