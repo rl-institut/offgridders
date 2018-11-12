1c52e1642d00c449db69398d851b91f981f688b6
smartie2076 <martha.hoffmann@rl-institut.de>
2018-11-12 14:44:26 +0100

    Class for sensitivity analysis

--- sensitivity.py
+++ sensitivity.py
@@ -7,51 +7,75 @@ dictonary, including filenames.
 import itertools
 import numpy as np
 
-# values of the sensitivity analysis that are variable
-sensitivity_bounds={
-    'fuel_price':     {'min': 0.5,  'max': 1,     'step': 0.1},
-    'shortage':       {'min': 0.5,  'max': 1,     'step': 0.1}
+
+class sensitivity():
+    def experiments(sensitivity_bounds, constant_values):
+        # create empty dictionary
+        dictof_oemparameters = {}
+
+        # fill dictionary with all sensitivity ranges defining the different simulations of the sensitivity analysis
+        # ! do not use a key two times, as it will be overwritten by new information
+        for keys in sensitivity_bounds:
+            if sensitivity_bounds[keys]['min'] == sensitivity_bounds[keys]['max']:
+                dictof_oemparameters.update({keys: np.array([sensitivity_bounds[keys]['min']])})
+            else:
+                dictof_oemparameters.update({keys: np.arange(sensitivity_bounds[keys]['min'],
+                                                             sensitivity_bounds[keys]['max'],
+                                                             sensitivity_bounds[keys]['step'])})
+        # fill dictionary with all constant values defining the different simulations of the sensitivity analysis
+        # ! do not use a key two times or in sensitivity_bounds as well, as it will be overwritten by new information
+        for keys in constant_values:
+            dictof_oemparameters.update({keys: np.array([constant_values[keys]])})
+
+        # create all possible combinations of sensitive parameters
+        keys, values = zip(*dictof_oemparameters.items())
+        experiments = [dict(zip(keys, v)) for v in itertools.product(*values)]
+        print(str(len(experiments)) + ' simulations are necessary to perform the sensitivity analysis.')
+
+        # define file postfix to save simulation
+        for i in range(0, len(experiments)):
+            filename = 's'
+            for keys in experiments[i]:
+                filename = filename + '_' + keys + '_' + str(experiments[i][keys])
+            filename
+            experiments[i].update({'filename': filename})
+
+        return experiments
+
+# constants/values of the sensitivity analysis - influencing the base case OEM
+oem = {
+    'sensitivity_bounds':
+        {
+            #'fuel_price':     {'min': 0.5,  'max': 1,     'step': 0.1}
+        },
+    'constant_values':
+        {
+            'cost_pv':      300,
+            'cost_genset':  400,
+            'cost_storage': 170,
+            'fuel_price':   1,
+            'shortage':     0,
+            'wacc':         0.05
+        }
     }
 
-# Values of the sensitivity analysis that appear constant
-constant_values = {
-    'cost_pv': 300,
-    'cost_genset': 400,
-    'cost_storage': 170,
-    'wacc':           0.05
+# constants/variables of the sensitivity analysis - not influencing the base case OEM
+
+nooem = {
+    'sensitivity_bounds':
+        {
+            #'blackout_duration':     {'min': 2,  'max': 2.5,     'step': 0.5}
+        },
+    'constant_values':
+        {
+            'blackout_frequency':   7,
+            'blackout_duration':    2,
+            'distance_to_grid':     10
+        }
     }
 
-#dictof_nooemparameters ={'blackout_duration_100':      {min: 2,  max: 4,     step: 0.5},
-#                       'blackout_frequency_100':       {min: 10,  max: 10,   step: 1},
-#                       'distance_to_grid':             {min: 10,  max: 10,   step: 10}}
+oem_experiments=sensitivity.experiments(oem['sensitivity_bounds'], oem['constant_values'])
+print (oem_experiments[0])
 
-dictof_oemparameters = {}
-
-for keys in sensitivity_bounds:
-    print(keys)
-    if sensitivity_bounds[keys]['min'] == sensitivity_bounds[keys]['max']:
-        dictof_oemparameters.update({keys: np.array([sensitivity_bounds[keys]['min']])})
-    else:
-        dictof_oemparameters.update({keys: np.arange(sensitivity_bounds[keys]['min'],
-                                                     sensitivity_bounds[keys]['max'],
-                                                     sensitivity_bounds[keys]['step'])})
-
-for keys in constant_values:
-    print(keys)
-    dictof_oemparameters.update({keys: np.array([constant_values[keys]])})
-
-print (dictof_oemparameters)
-keys, values = zip(*dictof_oemparameters.items())
-experiments = [dict(zip(keys, v)) for v in itertools.product(*values)]
-# number of simulations:
-print(str(len(experiments))+ ' simulations are necessary to perform the sensitivity analysis')
-
-
-for i in range(0, len(experiments)):
-    filename = 's'
-    for keys in experiments[i]:
-        filename = filename + '_' + keys+'_'+str(experiments[i][keys])
-    filename
-    experiments[i].update({'filename': filename})
-
-print (experiments[0])
+nooem_experiments=sensitivity.experiments(nooem['sensitivity_bounds'], nooem['constant_values'])
+print(nooem_experiments[0])
