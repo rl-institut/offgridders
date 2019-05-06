617e42bbf140d62d4ce83751cb5650f7b357a997
Martha_RLI <martha.hoffmann@rl-institut.de>
2019-05-06 10:31:06 +0200

    script for nigeria

--- standalone_scripts/script_identify_clusters.py
+++ standalone_scripts/script_identify_clusters.py
@@ -6,10 +6,19 @@ data_set_ids = pd.read_csv(path, sep=';')
 path = "/home/local/RL-INSTITUT/martha.hoffmann/rl-institut/05_Temp/Catherina/Martha/Model_final/Loads/high_tariff_new_nesp_id.csv"
 data_set = pd.read_csv(path, sep=';').drop(columns=['load_ID'])
 
+print (len(data_set.columns))
+print (len(data_set.index))
+
+print (len(data_set_ids.columns))
+print (len(data_set_ids.index))
+
 
 timeseries = pd.DataFrame(index=[i for i in range(0,8760)])
 
 for item in data_set_ids.index:
-    timeseries[data_set_ids["NESP_ID"][item]]=data_set[[str(data_set_ids["NESP_ID"][item])]]
+    timeseries[data_set_ids["NESP_ID"][item]]=data_set[str(data_set_ids["NESP_ID"][item])]
+
+print (len(timeseries.columns))
+print (len(timeseries.index))
 
 timeseries.to_csv('./demand_profiles_nigeria_plateau.csv')
