da54076d453eddcf3ae6a07cab3f4e6412739463
Martha_RLI <martha.hoffmann@rl-institut.de>
2019-05-21 17:25:22 +0200

    Versionierung 3.0

--- A_main_script.py
+++ A_main_script.py
@@ -18,8 +18,8 @@ logger.define_logging(logpath='./',
                       #screen_level=logging.DEBUG,
                       file_level=logging.DEBUG)
 
-logging.info('\n \n MICRO GRID TOOL 2.2 '
-             '\n Version: 16.05.2019 '
+logging.info('\n \n MICRO GRID TOOL 3.0 '
+             '\n Version: 21.05.2019 '
              '\n Coded by: Martha M. Hoffmann '
              '\n Reiner Lemoine Institute (Berlin) \n \n ')
 
--- multiple_simulations.sh
+++ multiple_simulations.sh
@@ -8,20 +8,20 @@ python3 A_main_script.py ./inputs/reliable_stage1_mgs.xlsx
 python3 A_main_script.py ./inputs/reliable_stage2_mgs.xlsx
 python3 A_main_script.py ./inputs/reliable_stage3_mgs.xlsx
 
-python3 A_main_script.py ./inputs/shs_stage1_mgs.xlsx
-python3 A_main_script.py ./inputs/shs_stage2_mgs.xlsx
-python3 A_main_script.py ./inputs/shs_stage3_mgs.xlsx
+#python3 A_main_script.py ./inputs/shs_stage1_mgs.xlsx
+#python3 A_main_script.py ./inputs/shs_stage2_mgs.xlsx
+#python3 A_main_script.py ./inputs/shs_stage3_mgs.xlsx
 
-python3 A_main_script.py ./inputs/unreliable_stage1_mgs.xlsx
-python3 A_main_script.py ./inputs/unreliable_stage2_mgs.xlsx
-python3 A_main_script.py ./inputs/unreliable_stage3_mgs.xlsx
+#python3 A_main_script.py ./inputs/unreliable_stage1_mgs.xlsx
+#python3 A_main_script.py ./inputs/unreliable_stage2_mgs.xlsx
+#python3 A_main_script.py ./inputs/unreliable_stage3_mgs.xlsx
 
 
-python3 A_main_script.py ./inputs/shs_shortage_stage1_mgs.xlsx
-python3 A_main_script.py ./inputs/shs_shortage_stage2_mgs.xlsx
-python3 A_main_script.py ./inputs/shs_shortage_stage3_mgs.xlsx
+#python3 A_main_script.py ./inputs/shs_shortage_stage1_mgs.xlsx
+#python3 A_main_script.py ./inputs/shs_shortage_stage2_mgs.xlsx
+#python3 A_main_script.py ./inputs/shs_shortage_stage3_mgs.xlsx
 
 python3 A_main_script.py ./inputs/reliable_no_mgs.xlsx
-python3 A_main_script.py ./inputs/shs_shortage_no_mgs.xlsx
+#python3 A_main_script.py ./inputs/shs_shortage_no_mgs.xlsx
 python3 A_main_script.py ./inputs/shs_no_mgs.xlsx
-python3 A_main_script.py ./inputs/unreliable_no_mgs.xlsx
+#python3 A_main_script.py ./inputs/unreliable_no_mgs.xlsx
