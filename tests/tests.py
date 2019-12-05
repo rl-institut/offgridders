3ba23f7f8da1609fc21de29ad6db5dd27267a77e
smartie2076 <gitMA18rli>
2019-12-05 14:31:18 +0100

    Add new pytests
    - termination test
    - blacks tests on both main and code folder

---
+++ tests/tests.py
@@ -0,0 +1,17 @@
+import pytest
+import os
+
+from ..A0_main_script import main
+
+def test_execution_not_terminated():
+    assert main()==1
+
+def test_blacks_main():
+    # Testing code formatting in main folder
+    r = os.system("black --check *.py")
+    assert r == 0
+
+def test_blacks_code_folder():
+    # Testing code formatting in code folder
+    r = os.system("black --check code_folder/*.py")
+    assert r == 0
