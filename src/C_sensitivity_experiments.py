34ccc09e3dd65c9a823a9121a18c7195ef587252
smartie2076 <gitMA18rli>
2020-06-30 10:54:21 +0200

    Fix wrong parameter call (capacity_inverter_dc_ac_kW vs capacity_inverter_kW)

--- src/C_sensitivity_experiments.py
+++ src/C_sensitivity_experiments.py
@@ -826,7 +826,7 @@ def overall_results_title(
                     "capacity_storage_kWh",
                     "power_storage_kW",
                     "capacity_rectifier_ac_dc_kW",
-                    "capacity_inverter_kW",
+                    "capacity_inverter_dc_ac_kW",
                     "capacity_wind_kW",
                     "capacity_genset_kW",
                     "capacity_pcoupling_kW",
