d580de60788c9b9fd3d4aa8b2267df60b019a3a3
Santiago infantino <santiago.infantino.moreno@gmail.com>
2020-07-17 13:09:46 +0200

    Rename "total_demand_excess_dc_annual_kWh"

--- src/G3_oemof_evaluate.py
+++ src/G3_oemof_evaluate.py
@@ -177,7 +177,7 @@ def get_excess(
         ]
         excess += excess_dc
         annual_value(
-            "total_demand_excess_dc_annual_kWh", excess, oemof_results, case_dict
+            TOTAL_DEMAND_EXCESS_DC_ANNUAL_KWH, excess, oemof_results, case_dict
         )  # not given as result.csv right now
         e_flows_df = join_e_flows_df(
             excess_dc, "Excess generation DC", e_flows_df
